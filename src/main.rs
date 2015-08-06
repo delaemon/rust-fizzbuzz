9f1b7b7c8dc291690b8e70ec8ab973d0f1cf2d30
delaemon <takeshi.music.5@gmail.com>
2015-08-07 08:48:37 +0900

    use pattern matching

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
 fn main() {
-    for i in 0..30 {
-        let fizz = if i % 3 == 0 { "fizz" } else { "" };
-        let buzz = if i % 5 == 0 { "buzz" } else { "" };
-        println!("{} {}{}", i, fizz, buzz);
-    }
+	for n in 0..30 {
+		// println!("{}", fizzbuzz1(n));
+		println!("{}", fizzbuzz2(n));
+	}
+}
+
+fn fizzbuzz1(n:i32) -> String {
+	let fizz = if n % 3 == 0 { "fizz" } else { "" };
+	let buzz = if n % 5 == 0 { "buzz" } else { "" };
+	return n.to_string() + fizz + buzz;
+}
+
+fn fizzbuzz2(n:i32) -> String {
+	let fizz = match n % 3 {
+			0 => "fizz",
+			_ => "",
+		};
+	let buzz = match n % 5 {
+			0 => "buzz",
+			_ => "",
+		};
+	return n.to_string() + fizz + buzz;
 }
